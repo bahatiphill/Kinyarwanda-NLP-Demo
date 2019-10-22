d1c36d43c2ff9ed08391baeef2018b9d1cdf3b6f
phill <phill@localhost.localdomain>
2019-10-22 11:32:30 +0200

    getting all paragraphs from the single article

--- kinyarwandaNLP-demo.py
+++ kinyarwandaNLP-demo.py
@@ -23,6 +23,25 @@ def get_latest_articles(url):
 		print("")
 	return LINKS
 
+
+def scrape_single_article(link):
+	response = requests.get(link)
+	soup = BeautifulSoup(response.text, 'html.parser')
+
+	article_body = soup.find_all("div", class_="wrapart")[1]
+	
+	paragraphs =  article_body.find_all('p')
+	paragraphs =  [paragraph.text for paragraph in paragraphs]
+	print("")
+	print("PRINT Paragraphs from the Article")
+	print('**********************************')
+	for par in paragraphs:
+		print(par)
+		print("")
+	return paragraphs
+
+
+
 def main():
 	print("Getting started......")
 	print("")
@@ -32,10 +51,12 @@ def main():
 
 	random_article_to_scrape = random.randint(0, len(articles)-1)
 	random_article_to_scrape = articles[random_article_to_scrape]
+	random_article_to_scrape = "http://umuryango.rw/"+random_article_to_scrape
 	
 	print("")
-	print("Going to Scrape --> ", "http://umuryango.rw/"+random_article_to_scrape)
+	print("Going to Scrape --> ", random_article_to_scrape)
 	print("##################################################################################")
+	scrape_single_article(random_article_to_scrape)
 
 if __name__ == "__main__":
     main()
